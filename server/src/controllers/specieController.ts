ab7b1d5fefc9fab140ce7356cb3a00b5a517fe42
Sarmad Raees Khan <khan@3einformatica.it>
2024-07-26 01:30:45 +0200

    update controller

--- server/src/controllers/specieController.ts
+++ server/src/controllers/specieController.ts
@@ -1,50 +1,34 @@
 import { Request, Response } from 'express';
 import { saveSpecies, getAllSpecies  } from '../services/dbService';
-import { Specie } from '../models/specie';
-import { fetchConservationMeasures, fetchRegions, fetchSpeciesByRegion } from '../services/apiService';
-import { getRandomElement } from '../utilities/utils';
+import { fetchRegions, getRandomRegion, fetchSpeciesByRegion, filterCriticallyEndangered, filterMammals } from '../services/apiService';
 
-const filterCriticallyEndangered = async (speciesList: Specie[]): Promise<Specie[]> => {
-  const criticallyEndangeredSpecies = await Promise.all(speciesList
-    .filter(species => species.status === 'CRITICALLY ENDANGERED')
-    .map(async species => {
-      const measures: { title: string }[] = await fetchConservationMeasures(species.id);
-      const conservationMeasures = measures.map(m => m.title).join(', ');
-      const speciesData: Specie = { ...species, conservationMeasures };
-      saveSpecies(speciesData);
-      return speciesData;
-    })
-  );
-  return criticallyEndangeredSpecies;
-};
+export const getSpeciesData = async (req: Request, res: Response) => {
+  try {
+    const regions = await fetchRegions();
+    const randomRegion = getRandomRegion(regions);
+    
+    const speciesList = await fetchSpeciesByRegion(randomRegion.identifier);
+    const criticallyEndangeredSpecies = await filterCriticallyEndangered(speciesList);
+    const mammals = filterMammals(speciesList);
 
-const filterMammals = (speciesList: Specie[]): Specie[] => {
-  return speciesList.filter(species => species.class === 'MAMMALIA');
+    for (const species of criticallyEndangeredSpecies.slice(0, 15)) {
+      await saveSpecies(species);
+    }
+
+    res.json({
+      criticallyEndangered: criticallyEndangeredSpecies,
+      mammals
+    });
+  } catch (error:any) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getSavedSpecies = async (req: Request, res: Response) => {
   try {
     const speciesList = await getAllSpecies();
     res.json(speciesList);
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-export const getSpeciesData = async (req: Request, res: Response) => {
-  try {
-    const regions = await fetchRegions();
-    const randomRegion = getRandomElement(regions);
-    const speciesList = await fetchSpeciesByRegion(randomRegion.name);
-
-    const criticallyEndangeredSpecies = await filterCriticallyEndangered(speciesList);
-    const mammals = filterMammals(speciesList);
-
-    res.json({
-      criticallyEndangered: criticallyEndangeredSpecies,
-      mammals
-    });
-  } catch (error: any) {
+  } catch (error:any) {
     res.status(500).json({ error: error.message });
   }
 };
