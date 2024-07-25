d30df9e22577aa5a62ebd01d74232d2429b74dd0
Sarmad Raees Khan <khan@3einformatica.it>
2024-07-25 20:52:18 +0200

    add method to fetch data from db

--- server/src/services/dbService.ts
+++ server/src/services/dbService.ts
@@ -1,5 +1,6 @@
 import sqlite3 from 'sqlite3';
-import { Specie} from '../models/specie';
+import { Specie } from '../models/specie';
+import { promisify } from 'util';
 sqlite3.verbose();
 
 const db = new sqlite3.Database('./db/database.sqlite');
@@ -11,16 +12,29 @@ export const initDB = () => {
       name TEXT,
       region TEXT,
       status TEXT,
+      class TEXT,
       conservationMeasures TEXT
     )
   `);
 };
+const runAsync = promisify(db.run.bind(db));
+const allAsync = promisify(db.all.bind(db));
 
-export const saveSpecies = (species: Specie) => {
+export const saveSpecies = (specie: Specie) => {
   const sqlquery = db.prepare(`
-    INSERT OR REPLACE INTO species (id, name, region, status, conservationMeasures)
-    VALUES (?, ?, ?, ?, ?)
+    INSERT OR REPLACE INTO species (id, name, region, status, class, conservationMeasures)
+    VALUES (?, ?, ?, ?, ?, ?)
   `);
-  sqlquery.run(species.id, species.name, species.region, species.status, species.conservationMeasures);
+  sqlquery.run(specie.id, specie.name, specie.region, specie.status, specie.class, specie.conservationMeasures);
   sqlquery.finalize();
+};
+
+export const getAllSpecies = async (): Promise<Specie[]> => {
+  try {
+    const rows = await allAsync('SELECT * FROM species');
+    return rows as Specie[];
+  } catch (err) {
+    console.error('Error retrieving species:', err);
+    throw err;
+  }
 };
